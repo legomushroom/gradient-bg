2027eb7477700035a9bff88644b683f246205abe
sol0mka <legomushroom@gmail.com>
2014-04-28 23:03:44 +0300

    working on lines

--- js/main.coffee
+++ js/main.coffee
@@ -2,13 +2,14 @@ class Main
   constructor:(@o={})->
     @vars()
     @animateRainbow()
-    @animateProgress()
+    # @animateProgress()
+    @showIphone()
 
   vars:->
     @currentProgress = 2000
-    @rainbow      = document.getElementById('rainbow')
-    @process      = document.getElementById('js-process')
-    @processText  = document.getElementById('js-process-text')
+    @rainbow      = document.getElementById('gradient-pattern')
+    @mask         = document.getElementById('mask2')
+    @shape         = document.getElementById('js-shape')
     @scanImage    = document.getElementById('js-scan-image')
 
   animateRainbow:->
@@ -18,16 +19,30 @@ class Main
       repeat: -1
       yoyo: true
       onUpdate: (e)->
-        it.rainbow.setAttribute 'transform', 'rotate(' + @target.deg + ', 500, 500)'
+        attr = "rotate(#{@target.deg}, 500, 500)"
+        it.rainbow.setAttribute 'transform', attr
 
   animateProgress:->
     it = @
     i = -400
-    scan = document.getElementById('js-scan-image')
     tween = TweenMax.to { progress: -400 }, 10,
       progress: 1200
       repeat: -1
       onUpdate: (e)->
-        scan.setAttribute('y', @target.progress)
+        it.scanImage.setAttribute('y', @target.progress)
+
+  showIphone:->
+    for child in @shape.children
+    # child = @shape.children[3]
+      length = child.getTotalLength?()
+      child.setAttribute 'stroke-dasharray', length
+      child.setAttribute 'stroke-dashoffset', length
+      do(child, length)->
+        setTimeout ->
+          tween = TweenMax.to { p: length }, 2,
+            p: 0
+            onUpdate: ()->
+              child.setAttribute('stroke-dashoffset', @target.p)
+        , 1000
 
 new Main
--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@
       this.o = o != null ? o : {};
       this.vars();
       this.animateRainbow();
-      this.animateProgress();
+      this.showIphone();
     }
 
     Main.prototype.vars = function() {
       this.currentProgress = 2000;
-      this.rainbow = document.getElementById('rainbow');
-      this.process = document.getElementById('js-process');
-      this.processText = document.getElementById('js-process-text');
+      this.rainbow = document.getElementById('gradient-pattern');
+      this.mask = document.getElementById('mask2');
+      this.shape = document.getElementById('js-shape');
       return this.scanImage = document.getElementById('js-scan-image');
     };
 
@@ -27,25 +27,52 @@
         repeat: -1,
         yoyo: true,
         onUpdate: function(e) {
-          return it.rainbow.setAttribute('transform', 'rotate(' + this.target.deg + ', 500, 500)');
+          var attr;
+          attr = "rotate(" + this.target.deg + ", 500, 500)";
+          return it.rainbow.setAttribute('transform', attr);
         }
       });
     };
 
     Main.prototype.animateProgress = function() {
-      var i, it, scan, tween;
+      var i, it, tween;
       it = this;
       i = -400;
-      scan = document.getElementById('js-scan-image');
       return tween = TweenMax.to({
         progress: -400
       }, 10, {
         progress: 1200,
         repeat: -1,
         onUpdate: function(e) {
-          return scan.setAttribute('y', this.target.progress);
+          return it.scanImage.setAttribute('y', this.target.progress);
         }
       });
+    };
+
+    Main.prototype.showIphone = function() {
+      var child, length, _i, _len, _ref, _results;
+      _ref = this.shape.children;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        child = _ref[_i];
+        length = typeof child.getTotalLength === "function" ? child.getTotalLength() : void 0;
+        child.setAttribute('stroke-dasharray', length);
+        child.setAttribute('stroke-dashoffset', length);
+        _results.push((function(child, length) {
+          return setTimeout(function() {
+            var tween;
+            return tween = TweenMax.to({
+              p: length
+            }, 2, {
+              p: 0,
+              onUpdate: function() {
+                return child.setAttribute('stroke-dashoffset', this.target.p);
+              }
+            });
+          }, 1000);
+        })(child, length));
+      }
+      return _results;
     };
 
     return Main;
